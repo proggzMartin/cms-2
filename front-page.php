0420490b821db7beed98afccd33d9364d70823f4
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-10-31 06:19:43 +0100

    Add Bootstrap css, add text to frontpage for test.

--- front-page.php
+++ front-page.php
@@ -2,4 +2,16 @@
 
 <?php get_header();?>
 dsfgdfgf
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      left
+    </div>
+    <div class="col">
+      right
+    </div>
+  </div>
+</div>
+
 <?php:get_footer();?>
--- functions.php
+++ functions.php
@@ -1,2 +1,27 @@
-<!-- file to add hooks into, configure theme. -->
+<!-- file to add hooks into, configure theme, enque imports(?). -->
+
+
+<?php
+
+function load_stylesheets() {
+  //My own style
+  // wp_register_style(
+  //   'stylesheet', 
+  //   get_template_directory_uri() . '/bootstrapcss/bootstrap.min.css',
+  //   array(),
+  //   false, //version
+  //   'all');
+  // wp_enqueue_style('stylesheet');
+
+  //Bootstrap
+  wp_register_style(
+    'stylesheet', 
+    get_template_directory_uri() . '/bootstrapcss/bootstrap.min.css',
+    array(),
+    false, //version
+    'all');
+  wp_enqueue_style('stylesheet');
+}
+
+add_action('wp_enqueue_scripts', 'load_stylesheets');
 
