95ebf20a6479877d012489da84e12aac8c448794
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-10-31 16:36:10 +0100

    Added variable-fetch and image to front page.

--- front-page.php
+++ front-page.php
@@ -1,21 +1,28 @@
-<!-- template for front page -->
-
+<!-- Copied from page.php with a few changes. -->
 <?php get_header();?>
+
+<?php 
+  $title = get_field('page_title');
+  $description = get_field('page_description');
+  $link = get_field('page_link');
+  $imageArray = get_field('page_image');
+  $image = $imageArray['sizes']['large']; //var kommer den ifrån?
+?>
 
 <div class="container mt-5">
   <div class="row">
-    <div class="col-auto">
+      <img class="img img-fluid" src="<?php echo $image;?>">
+  </div>
+  <div class="row">
+    <div class="col">
       <!-- Dynamic display of title set in WP -->
-      <h1><?php the_title();?></h1>
+      <h1><?php echo $title?></h1>
     </div>
   </div>
   <div class="row">
     <div class="col">
-      <?php if(have_posts()) : while(have_posts()) : the_post();?>
-        <?php the_content();?>
-      <?php endwhile; endif;?>
+      <?php echo $description?>
     </div>
   </div>
 </div>
-
 <?php get_footer();?>
